db521694addcf46f6cbcecdae0e49b8e2868c08b
Kamp Duong <kodingkamp@gmail.com>
2019-12-20 13:54:48 -0500

    Adjusted game algorithm
    Typos:
    Fixed "retaliation" typo.
    Fixed missing enemy name.
    Changed HP scaling.
    Changed Attack scaling.
    Added chance of dodging attacks.
    Adjusted potion heal amount to scale.
    Added Potion Merchant that appears every 5 levels.
    Move database interact to a separate file.

--- public/assets/script/game/Enemy.js
+++ public/assets/script/game/Enemy.js
@@ -2,33 +2,25 @@ class Enemy {
     constructor(playerLevel) {
         this.level = playerLevel;
         this.type = enemyList[Math.floor(Math.random() * enemyList.length)];
-        this.maxPossibleHealth = enemyStartingMaxHealth + 2.5 * (this.level - 1);
+        this.maxPossibleHealth = enemyStartingMaxHealth + 3.5 * (this.level - 1);
         this.maxHealth = Math.ceil(Math.random() * this.maxPossibleHealth);
         this.health = this.maxHealth;
-        this.attack = enemyStartingMaxAttack + 1.3 * this.level * (this.level - 1);
-        appendToDisplay("A <bad-guy" + this.type + "</bad-guy> has appeared!", true);
-        console.log("Enemy's Attack: " + this.attack);
+        this.attack = enemyStartingMaxAttack + 1.006 * this.level * (this.level - 1);
+        appendToDisplay(`A <bad-guy>${this.type}</bad-guy> has appeared!`, true);
+        console.log(`Enemy's Attack: ${this.attack}`);
     }
 
-    get name() {
-        return this.type;
-    }
+    get name() { return this.type; }
 
-    get maxHP() {
-        return this.maxHealth;
-    }
+    get maxHP() {return this.maxHealth; }
 
-    get hp() {
-        return this.health;
-    }
+    get hp() { return this.health; }
 
-    power() {
-        return Math.ceil(Math.random() * this.attack);
-    }
+    power() { return Math.ceil(Math.random() * this.attack); }
 
     takeDamage(damage) {
         // Correct health underflow when taking damage.
         if((this.health -= damage) < 0) this.health = 0;
-        appendToDisplay("<hr>You strike the <bad-guy>" + this.type + "</bad-guy> for <dmg-dealt>" + damage + "</dmg-dealt> damage.");
+        appendToDisplay(`<hr>You strike the <bad-guy>${this.type}</bad-guy> for <dmg-dealt>${damage}</dmg-dealt> damage.`);
     }
 }
