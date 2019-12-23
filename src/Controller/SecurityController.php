811ff898bd2f2bdf236b8e8cc27a2906cec1dacf
Jason <jasonconnor@users.noreply.github.com>
2019-12-22 21:29:41 -0500

    Added User Registration Functionality
    Users can now register an account, are immediately signed in, and redirected to "play"
    cleaned up code and altered template so anonymous users can see the "play" link

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,21 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Security\LoginFormAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class SecurityController extends AbstractController
 {
     private $passwordEncoder;
 
-    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $em)
     {
         $this->passwordEncoder = $passwordEncoder;
+        $this->em = $em;
     }
 
     /**
@@ -45,29 +49,40 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/new-user", name="new_user")
+     * @Route("/register", name="register")
      */
-    public function newUser(EntityManagerInterface $em)
+    public function register(Request $request, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $formAuthenticator)
     {
-        $user = new User();
-        $user->setUsername('jason')
-            ->setPassword($this->passwordEncoder->encodePassword(
+        //todo - use Symfony Forms and Validation
+
+        if ($request->isMethod('POST')) {
+            $user = new User();
+            $user->setUsername($request->get('username'));
+
+            $user->setPassword($this->passwordEncoder->encodePassword(
                 $user,
-                '1234'
-            ))
-        ;
+                $request->get('password')
+            ));
 
-        $em->persist($user);
-        $em->flush();
+            $this->em->persist($user);
+            $this->em->flush();
+            
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $formAuthenticator,
+                'main'
+            );
+        }        
 
-        return new Response(sprintf('Successfully added user %s', $user->getUsername()));
+        return $this->render('security/register.html.twig');
     }
 
     /**
      * @Route("/new-admin", name="new_admin")
      * @IsGranted("ROLE_ADMIN")
      */
-    public function newAdmin(EntityManagerInterface $em)
+    public function newAdmin()
     {
         $user = new User();
         $user->setUsername('admin2')
@@ -78,8 +93,8 @@ class SecurityController extends AbstractController
             ))
         ;
 
-        $em->persist($user);
-        $em->flush();
+        $this->em->persist($user);
+        $this->em->flush();
 
         return new Response(sprintf('Successfully added user %s', $user->getUsername()));
     }
