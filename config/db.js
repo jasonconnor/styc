454c727795951795f151fa7935d0c0c043c8ced3
Jason <jasonpconn@gmail.com>
2020-06-04 22:13:11 -0400

    Configured Connection to Local Dev Database

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const url = 'mongodb://127.0.0.1:27017/styc'
+
+const connect = async function() {
+    try {
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        console.log('Connected to the styc database.')
+    } catch(err) {
+        console.log(err.message)
+    }
+}
+
+module.exports = connect();
