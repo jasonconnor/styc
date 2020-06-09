dabf3f3d91184846258c16053935a8c78aea388c
Jason <jasonpconn@gmail.com>
2020-06-08 22:40:01 -0400

    Established Basic Routing
    Will soon add more routes (with appropriate methods) to each route file

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const app = express()
 // Connect to the database
 connect;
 
-app.get('/', function(req, res) {
-    res.send('Success')
-})
+// Routes
+app.use('/', require('./src/routes/app'))
+app.use('/admin', require('./src/routes/admin'))
+app.use('/account', require('./src/routes/account'))
+app.use('/news', require('./src/routes/news'))
 
 app.listen(80, function() {
     console.log('Server started.')
