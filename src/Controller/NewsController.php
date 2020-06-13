f535e8a447008b959ce15633dd4941edf0ea3e13
Jason Connor <jasonpconn@gmail.com>
2020-06-12 21:24:45 -0400

    Dynamic, Truncated Post Content Displayed on News

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Extra\String\StringExtension;
 
 class NewsController extends AbstractController
 {
