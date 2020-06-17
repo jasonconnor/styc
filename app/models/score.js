2becf002299f54b8947cf1e53ccb13efa2f474bd
Jason <jasonpconn@gmail.com>
2020-06-17 19:30:07 -0400

    Schema Update

--- app/models/score.js
+++ app/models/score.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const ScoreSchema = new mongoose.Schema({
+const ScoreSchema = new Schema({
     score: {
         type: Number,
         required: true,
