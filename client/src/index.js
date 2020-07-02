8248d0634fe899031268be7a8b4c6725c4ada0e0
Jason <jasonpconn@gmail.com>
2020-07-02 18:32:56 -0400

    Cleaned up files in Client directory

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Header from './components/header'
-import Footer from './components/footer'
-import RegisterForm from './components/forms/register'
-import './index.css'
 
 ReactDOM.render(
-  <React.Fragment>
-    <Header />
-    <RegisterForm />
-    <Footer />
-  </React.Fragment>,
+  <div>React</div>,
   document.getElementById('root')
-);
+)
