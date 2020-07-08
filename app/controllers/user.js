7c7aaf4955b13085931a951ccbbffa9a117f930b
Jason <jasonpconn@gmail.com>
2020-07-07 21:21:49 -0400

    Added Function to Register User

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,5 +1,5 @@
+const bcrypt = require('bcrypt')
 const { body, validationResult } = require('express-validator')
-
 
 const User = require('../models/user')
 
@@ -43,8 +43,19 @@ exports.register = [
   if(!errors.isEmpty()) {
     console.log(errors.array())
   } else {
+    try {
+      const { username, email, password } = req.body
+      const user = new User({ username, email, password })
 
+      const salt = await bcrypt.genSalt(10)
+      user.password = await bcrypt.hash(password, salt)
+    
+      await user.save()
+      
+      res.send('success')
+    } catch(error) {
+      console.error(error)
+      return next(error)
+    }
   }
-
-  res.send('Coming Soon: POST function for new users')
 }
