d6adf276c9473ae686758451db5a89a2110c162c
Jason <jasonpconn@gmail.com>
2020-12-19 12:46:58 -0500

    Added Extra Score Values to Score Model

--- src/models/ScoreModel.js
+++ src/models/ScoreModel.js
@@ -11,10 +11,62 @@ const ScoreSchema = new mongoose.Schema({
   },
   score: {
     type: Number,
-    default: 0,
+    default: 0
   },
   level: {
     type: Number,
-    default: 0,
+    default: 0
+  },
+  enemiesSlain: {
+    type: Number,
+    default: 0
+  },
+  attacksDealt: {
+    type: Number,
+    default: 0
+  },
+  criticalsDealt: {
+    type: Number,
+    default: 0
+  },
+  damageDealt: {
+    type: Number,
+    default: 0
+  },
+  mostDamageDealt: {
+    type: Number,
+    default: 0
+  },
+  attacksTaken: {
+    type: Number,
+    default: 0
+  },
+  damageTaken: {
+    type: Number,
+    default: 0
+  },
+  mostDamageTaken: {
+    type: Number,
+    default: 0
+  },
+  attacksDodged: {
+    type: Number,
+    default: 0
+  },
+  potionsUsed: {
+    type: Number,
+    default: 0
+  },
+  pointsSpent: {
+    type: Number,
+    default: 0
+  },
+  healthRestored: {
+    type: Number,
+    default: 0
+  },
+  timesRan: {
+    type: Number,
+    default: 0
   }
 })
