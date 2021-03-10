01c411342649c0f278de1286541b4cd01d62a1f7
Jason <jasonpconn@gmail.com>
2021-03-09 22:05:56 -0500

    Added an onSubmit Method for LoginForm Component

--- client/src/components/Forms/LoginForm.js
+++ client/src/components/Forms/LoginForm.js
@@ -1,18 +1,55 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import FormInput from './FormInput';
 
 export default function LoginForm() {
-  const { errors } = useForm({ mode: 'onChange' });
+  const [formError, setFormError] = useState('');
+  const { errors, handleSubmit, register } = useForm({ mode: 'onChange' });
+
+  async function onSubmit(data) {
+    const formData = new FormData();
+
+    formData.append('username', data.username);
+    formData.append('password', data.password);
+
+    let response = null;
+    let result = null;
+
+    try {
+      response = await fetch('http://localhost:80/login', {
+        method: 'POST',
+        body: formData,
+      });
+
+      result = await response.json();
+    } catch (error) {
+      setFormError(error.message);
+    }
+
+    if (result === null) {
+      setFormError('Received empty response from the server. Try again later.');
+    } else if (result.hasOwnProperty('error')) {
+      setFormError(result.error);
+    }
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {formError ? <div>{formError}</div> : null}
+
       <FormInput
         type='text'
         name='username'
         label='Username:'
         placeholder='Username'
         error={errors?.username?.message}
+        ref={register({
+          required: {
+            value: true,
+            message: 'Username is required to log in.',
+          },
+        })}
       />
 
       <FormInput
@@ -21,6 +58,12 @@ export default function LoginForm() {
         label='Password:'
         placeholder='Password'
         error={errors?.password?.message}
+        ref={register({
+          required: {
+            value: true,
+            message: 'Password is required to log in.',
+          },
+        })}
       />
 
       <input type='submit' value='Login' />
