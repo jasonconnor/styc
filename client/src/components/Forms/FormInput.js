f5071d59fd719937f1f93ed08b0053efdfa8e90a
Jason <jasonpconn@gmail.com>
2021-03-10 22:33:11 -0500

    Refactored FormInput to Submit Form Data to API

--- client/src/components/Forms/FormInput.js
+++ client/src/components/Forms/FormInput.js
@@ -1,4 +1,6 @@
-export default function FormInput(props) {
+import React from 'react';
+
+const FormInput = React.forwardRef((props, ref) => {
   return (
     <div className='formInput'>
       <label>{props.label}</label>
@@ -7,10 +9,13 @@ export default function FormInput(props) {
         type={props.type}
         name={props.name}
         placeholder={props.placeholder}
+        ref={ref}
         autoComplete='off'
       />
 
       <span>{props.error}</span>
     </div>
   );
-}
+});
+
+export default FormInput;
