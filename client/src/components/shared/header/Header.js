7d6e4b785605a20d8a48caf7eaf496cff9561d28
Kamp Duong <kodingkamp@gmail.com>
2021-03-20 17:44:25 -0400

    Refactored RouteDict
    RouteDict can now be used to specify more than just header's background color.
    
    Can be used for other properties and other components.

--- client/src/components/shared/header/Header.js
+++ client/src/components/shared/header/Header.js
@@ -1,36 +1,59 @@
 import React from 'react';
-import { useLocation, Link } from 'react-router-dom'
-import { Grid, makeStyles, Button } from '@material-ui/core'
-import routeDict from './RouteDict'
+import { useLocation, Link } from 'react-router-dom';
+import { Grid, makeStyles, Button } from '@material-ui/core';
+import { HeaderRouteDict } from 'models/route-dictionary/RouteDict';
 
+/* Harness: 
+import Header from "components/shared/header/Header";
+
+<Header />
+*/
+
+/**
+ * Header Page Component
+ */
 export default function Header() {
   // Get current route
   const route = useLocation().pathname;
 
-  // Returns the header color based on current route
-  const getHeaderColor = () => {    
-    // If the route key exists
-    if (Object.keys(routeDict).includes(route)) {
-      return routeDict[route]
-    } else { // route key does not exist
-      return 'white'
-    }
+  /**
+   * Get the specified background color depending on path.
+   * @returns string - css background-color, (default) 'white'
+   */
+  const getHeaderBackgroundColor = () => {   
+    return Object.keys(HeaderRouteDict).includes(route)
+      // If the current path is defined in the dictionary
+      ? HeaderRouteDict[route].BGColor
+      // path is not defined in the route dictionary
+      : 'white';
+  }
+
+  /**
+   * Get the specified position value depending on path.
+   * @returns string - css position, (default) 'inherit'
+   */
+   const getHeaderPosition = () => {
+    return Object.keys(HeaderRouteDict).includes(route)
+      //if the current path is defined in the dictionary  
+      ? HeaderRouteDict[route].Position
+      // path is not defined in the route dictionary
+      : 'inherit';
   }
 
   // ~ MUI ~
   const useStyles = makeStyles(theme => ({
     header: {
-      position: 'absolute',
+      position: getHeaderPosition(),
       paddingLeft: theme.spacing(5),
       paddingRight: theme.spacing(5),
-      background: getHeaderColor()
+      background: getHeaderBackgroundColor()
     },
 
     link: {
       textDecoration: 'none', 
       fontWeight: 'bold', 
       color:'black',
-    }
+    },
   }));
 
   const classes = useStyles();
