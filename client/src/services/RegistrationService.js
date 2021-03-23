18bfa42fa49673c0785b2923eb4d56479583dd79
Jason <jasonpconn@gmail.com>
2021-03-22 21:59:41 -0400

    Added Registration Service

---
+++ client/src/services/RegistrationService.js
@@ -0,0 +1,54 @@
+import ErrorMessage from 'models/messaging/ErrorMessages';
+
+/* Harness:
+import RegistrationSerivce from 'services/RegistrationService;
+
+RegstrationService.registerUser(data)
+*/
+
+/**
+ * Service - Handles registration requests & responses with API
+ */
+
+export default class RegistrationSerivce {
+  static registerUser = (data) => {
+    const formData = new FormData();
+
+    formData.append('username', data.username);
+    formData.append('passowrd', data.password);
+
+    const postRequestOptions = {
+      method: 'POST',
+      body: formData,
+    };
+
+    return new Promise(async (resolve, reject) => {
+      try {
+        /* Sends the POST Request */
+        const response = await fetch(
+          `${process.env.SERVER_URI}/api/register`,
+          postRequestOptions
+        ).catch(() => {
+          throw new Error(ErrorMessage.NetworkError);
+        });
+
+        const responseJson = await response.json();
+
+        /* Check for errors */
+        // Check for empty response {}
+        if (!responseJson || Object.keys(responseJson).length === 0) {
+          throw new Error(ErrorMessage.EmptyResponse);
+        }
+
+        if (responseJson.hasOwnProperty('error')) {
+          throw new Error(responseJson.error);
+        }
+
+        resolve(responseJson);
+      } catch (error) {
+        // Reject the Promise with the friendly error
+        reject(error.message);
+      }
+    });
+  };
+}
