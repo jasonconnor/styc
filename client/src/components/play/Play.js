ca9361592aecf45b5d4ebd89e5c21d67fe635119
Kamp Duong <kodingkamp@gmail.com>
2021-03-24 21:13:26 -0400

    PlayTest

--- client/src/components/play/Play.js
+++ client/src/components/play/Play.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Grid, makeStyles } from '@material-ui/core'
+import React, { useState } from 'react';
+import { Grid, makeStyles, ButtonGroup, Button } from '@material-ui/core'
 import { blueGrey } from '@material-ui/core/colors'
 /* Harness:
 import Play from 'components/play/Play';
@@ -7,10 +7,29 @@ import Play from 'components/play/Play';
 <Route exact path='/play' component={Play} />
 */
 
+const gameStateEnum = {
+  '0': "Main Menu",
+  '1': "Combat",
+  '2': "Post Combat"
+}
+
+const buttonMessages = {
+  '0': {
+    
+  },
+  '1': {
+
+  },
+  '2': {
+
+  }
+}
+
 /**
  * Play page component.
  */
 export default function Play() {
+  const [gameState, setGameState] = useState(0);
   
   // ~ MUI ~
   const useStyles = makeStyles(theme => ({
@@ -23,10 +42,20 @@ export default function Play() {
   const classes = useStyles();
 
   return (
-    <Grid container justify="center" alignItems="center" className={classes.root}>
-        <Grid item>
-            <label>Play page stuff</label>
-        </Grid>
+    <Grid container justify="center" alignItems="center" spacing={3} className={classes.root}>
+      <Grid item>
+        <label>Game State: {gameStateEnum[gameState]}</label>
+      </Grid>
+      <Grid item>
+        <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+          <Button onClick={_ => setGameState(0)}>Game State 0</Button>
+          <Button onClick={_ => setGameState(1)}>Game State 1</Button>
+          <Button onClick={_ => setGameState(2)}>Game State 2</Button>
+        </ButtonGroup>
+      </Grid>
+      <Grid item>
+        Some Words Here
+      </Grid>
     </Grid>
   )
 }
