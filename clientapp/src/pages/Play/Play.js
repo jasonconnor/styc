2181ff9ed074363b48cafc85d24f832effe5f0ae
Kamp <kodingkamp@gmail.com>
2022-10-26 22:19:52 -0400

    Add mui icons library and test icons

--- clientapp/src/pages/Play/Play.js
+++ clientapp/src/pages/Play/Play.js
@@ -1,9 +1,44 @@
+import { 
+  AddCircle,
+  ColorizeTwoTone, 
+  Favorite, 
+  FavoriteBorder,
+  Healing,
+  RemoveCircle
+} from '@mui/icons-material'
+import { Chip, Stack } from '@mui/material'
 import './play.scss'
 
 const Play = () => {
-  return (
-    <div class='play-page-container'>Play page</div>
-  )
+  return (<div class='play-page-container'>
+    <Stack spacing={2}>
+      <span>
+        <ColorizeTwoTone sx={{ transform: 'rotate(180deg)' }} />
+        Sword
+      </span>
+      <span>
+        <Favorite />
+        <FavoriteBorder />
+        Health
+      </span>
+      <span>
+        <Healing />
+        Healing
+      </span>
+      <Chip
+        avatar={<AddCircle />}
+        label='Boons'
+        sx={{width: 'fit-content'}}
+        className='boon'
+      />
+      <Chip
+        avatar={<RemoveCircle />}
+        label='Debuffs'
+        sx={{width: 'fit-content'}}
+        className='debuff'
+      />
+    </Stack>
+  </div>)
 }
 
 export default Play
