a73a52bb99e270f19610fe396bc35b3a3dd72c42
Kamp <kodingkamp@gmail.com>
2022-10-27 20:36:11 -0400

    More icons to consider

--- clientapp/src/components/Navbar/Navbar.js
+++ clientapp/src/components/Navbar/Navbar.js
@@ -1,24 +1,21 @@
+import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import './navbar.scss'
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul className='remove-this-later'>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/Leaderboard">Leaderboard</Link>
-        </li>
-        <li>
-          <Link to="/Play">Play</Link>
-        </li>
-        <li>
-          <Link to="/Login">Login</Link>
-        </li>
-      </ul>
+    <nav id='navbar'>
+      <Stack 
+        className='remove-this-later'
+        direction='row'
+        spacing={2}
+      >
+        <Link to="/">Home</Link>
+        <Link to="/Leaderboard">Leaderboard</Link>
+        <Link to="/Play">Play</Link>
+        <Link to="/Login">Login</Link>
+      </Stack>
     </nav>
   )
 }
