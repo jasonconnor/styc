75eed39bfac6fe224de0e1579e8639395f3451bc
Jason <jasonpconn@gmail.com>
2022-12-18 10:07:21 -0500

    Started on Leaderboards!

--- clientapp/src/pages/Leaderboard/Leaderboard.js
+++ clientapp/src/pages/Leaderboard/Leaderboard.js
@@ -1,8 +1,34 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 import './leaderboard.scss'
 
 const Leaderboard = () => {
   return (
-    <div>Leaderboard Page</div>
+    <div>
+      <h2>Leaderboards</h2>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 300 }} aria-label='simple table'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Username</TableCell>
+              <TableCell align='right'>Score</TableCell>
+              <TableCell align='right'>Enemies Slain</TableCell>
+              <TableCell align='right'>Date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   )
 }
 
