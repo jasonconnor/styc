7b400b765441d53ee04b8bda80f79b6637d134dd
Kamp Duong <kodingkamp@gmail.com>
2022-12-18 20:07:13 -0500

    Update import for leaderboard page

--- clientapp/src/pages/Leaderboard/Leaderboard.js
+++ clientapp/src/pages/Leaderboard/Leaderboard.js
@@ -1,10 +1,12 @@
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import { 
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from '@mui/material';
 
 import './leaderboard.scss'
 
