ce480a10fc72b9f98225b92c3d56b6d5794e38fb
Jason <jasonpconn@gmail.com>
2023-02-01 21:14:04 -0500

    Updated enemy service methods

--- api/src/game/enemies/enemies.service.js
+++ api/src/game/enemies/enemies.service.js
@@ -2,18 +2,8 @@ import { EnemiesModel } from './enemies.model.js'
 
 export async function getAllEnemies() {
   try {
-    const enemies = await EnemiesModel.findAll()
+    const enemies = await EnemiesModel.find()
     return [enemies, null]
-  } catch (error) {
-    console.error(error)
-    return [null, error]
-  }
-}
-
-export async function getEnemyByName(name) {
-  try {
-    const enemy = await EnemiesModel.findOne(name)
-    return [enemy, null]
   } catch (error) {
     console.error(error)
     return [null, error]
@@ -22,7 +12,7 @@ export async function getEnemyByName(name) {
 
 export async function getRandomEnemy() {
   try {
-    const enemy = await EnemiesModel.sample()
+    const enemy = await EnemiesModel.aggregate([{$sample: {size: 1}}])
     return [enemy, null]
   } catch (error) {
     console.error(error)
