eb6f71f1166fd48f8888b808efe83d31d57cebdc
Kamp Duong <kodingkamp@gmail.com>
2023-03-08 21:16:40 -0500

    Askey art

--- clientapp/src/pages/Play/PlayComponents/BackButton.js
+++ clientapp/src/pages/Play/PlayComponents/BackButton.js
@@ -3,6 +3,54 @@ import { IconButton } from '@mui/material'
 import { ArrowBack, AutoStories } from '@mui/icons-material'
 import colors from '../../../styles/_export.scss'
 
+/*
+            Goblin Rocket look back
+                 ,      ,
+                /(.-""-.)\
+            |\  \/      \/  /|
+            | \ / 0  0 \ / |
+            \(   ,\__/ , )/
+             \_,-\____/-,_/
+              /   \/ \/   \
+        ___/             \___
+      /`    \      /      | |
+     /       '----'       / /
+    (          __        | /
+     \_.-.___,'  `.___,.-'_/
+       /              ,___)
+      /                  \
+     (    )                \
+     /\___/                  \
+    /                         \
+   |                           |
+   |                           |
+   |                           |
+   |                           |
+   |                           |
+   |                           |
+   |                           |
+   |                           |
+   |                           |
+   |                           |
+   |                           |
+   |                           |
+   |                           |
+   |                           |
+   |                           |
+   |    (   )           (   )  |
+   |     \_/             \_/   |
+  _|_                         _|_
+ /   \                       /   \
+|     |                     |     |
+|     |                     |     |
+|     |                     |     |
+|     |                     |     |
+|     |                     |     |
+|_____|                     |_____|
+  |||                         |||
+ '---'                       '---' 
+*/
+
 const BackButton = (props) => {
   const {clickHandler} = props
 
